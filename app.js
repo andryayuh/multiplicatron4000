4202e911bd417a4b27f18b35451745a8ebaa8f50
Andrea Miralles <andreamiralles@andreamllessmbp.attlocal.net>
2017-04-18 22:24:51 -0700

    checkpoint commit

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-	const multiply = require('./multiplier.js');
+const multiply = require('./multiplier.js');
 	
-	console.log(multiply(5, 6));
+class ViewManager {
+	connectEventHandlers() {
+		document.getElementById('form-numbers')
+		.addEventListener(
+			'submit',
+			this.onSubmit);
+			//this.onSubmit.bind(this));
+	}
+	onSubmit(event) {
+		event.preventDefault();
+		let num1 = document.getElementById('input-num1').value;
+		let num2 = document.getElementById('input-num2').value;
+		num1 = parseInt(num1, 10);
+		num2 = parseInt(num2, 10);
+		const product = multiply(num1, num2);
+
+		//this.renderProduct(product);
+		alert(product);
+	}
+	//renderProduct(product) {
+	//	document.querySelector('.product').textContent = product;
+	//}
+}
+const viewManager = new ViewManager();
+viewManager.connectEventHandlers();
